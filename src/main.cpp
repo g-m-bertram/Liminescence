4f55fe886378a1783d28be037dd5f7c01e749315
Gabe <gabebertram@hotmail.com>
2026-03-12 15:23:43 -0500

    working 3d camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
 ﻿#include "Liminescence.h"
-#include "raylib.h"
+#include"raylib.h"
 
 int main()
 {
-    InitWindow(800, 600, "My Game");
+    InitWindow(800, 600, "Liminescence");
     SetTargetFPS(60);
+
+    Camera3D camera = {};
+    camera.position = { 0.f, 5.f, 10.f };
+    camera.target = { 0.f, 0.f, 0.f };
+    camera.up = { 0.f, 1.f, 0.f };
+    camera.fovy = 70.f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    DisableCursor();
 
     while (!WindowShouldClose())
     {
+        UpdateCamera(&camera, CAMERA_FREE);
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Raylib is working!", 300, 280, 20, DARKGRAY);
+
+        ClearBackground(SKYBLUE);
+
+        BeginMode3D(camera);
+        DrawGrid(20, 1.f);
+        EndMode3D();
+
+        DrawText("WASD to move, mouse to look", 10, 10, 20, BLACK);
+
         EndDrawing();
     }
 
