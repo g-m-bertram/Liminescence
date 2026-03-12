b87bdbbedbb767cfd922ec5dd53cd23ec2dae2fb
Gabe <gabebertram@hotmail.com>
2026-03-12 14:55:32 -0500

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "Liminescence.h"
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(800, 600, "My Game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Raylib is working!", 300, 280, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
